0e2783cda4ff19e546b1fbccf2d9aa68a48b2595
mohdmuhaimin90 <mohdmuhaimin90@gmail.com>
2026-07-15 10:10:27 +0800

    fix domain URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://your-homestay.vercel.app',
+  site: 'https://homestay-kenangan.vercel.app',
   integrations: [
     tailwind(),
     sitemap(),
